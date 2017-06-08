93072d4eeb383785f93edb3f8ada7dcdba2cec38
aguilerap.jc <evamargarita.villarreal@gmail.com>
2017-06-07 20:40:34 -0500

    imu con funciones para el modulo de navegacion

--- lib/imu.py
+++ lib/imu.py
@@ -6,139 +6,103 @@ from inspect import getmembers
 from pprint import pprint
 import lib.variables as var
 
+'''
+	imu
 
-#Initialize IMU classes objects
-vnSensor = VnSensor()
-compData = CompositeData()
-attitude = Attitude()
-packet = Packet()
-BOD = BinaryOutputRegister()
+	********************************
 
+		** init imu module connection **
+			params: 
+				None
+			return:
+				 None
+			call:
+				imu.init()
+
+		** get gps coordenates **
+			params: 
+				None
+			return:
+				dynamic array  = {
+					float longitud
+					float latitude
+				}
+			call:
+				get_gps_coords(); 
+
+		** get yaw (absolute orientation) **
+			params: 
+				None
+			return:
+				float degree
+			call:
+				get_yaw_orientation
+
+		** get gps velocity and acceleration **
+			params: 
+				None
+			return:
+				dynamic array  = {
+					vec3f acceleration
+					vec3f velocity
+				}
+			call:
+				get_gps_acceleration_velocity
+		
+	***
+'''
+vnSensor = None
+NORTH_YAW = 87.35199737548828;
+
+def init():
+	global vnSensor;
+	vnSensor = VnSensor();
+	vnSensor.connect(var.imuPort , 115200);
 
 def print_model():
-	if(var.imuPort == ''):
-		print('Error : GetSerialPort must be called before this function')
-	else:
-		vnSensor.connect(var.imuPort, 115200)
-		model = vnSensor.read_model_number()
-		print(model)
-		vnSensor.disconnect()
-################################################################################
-def test_function():
-	#vnSensor.connect(var.imuPort,115200)
-	ez = EzAsyncData.connect(var.imuPort, 115200)
-	##COmando teórico: $VNWRG,75,2,16,01,7FFF*XX
-	
-	#ez.sensor.write_async_data_output_type(VNGPS)
-	#ez.sensor.write_async_data_output_type(29)
-	#print(ez.sensor.read_async_data_output_type())
-	#cd = ez.current_data
-	#pprint(getmembers(cd))
-	#pprint(getmembers(ez.sensor.read_gps_solution_lla()))
+	return vnSensor.read_model_number();
 
-	#print(BOD.async_mode)	
-	imuMeasures        = ez.sensor.read_imu_measurements();
-	deltaThetaVelocity = ez.sensor.read_delta_theta_and_delta_velocity();
-	YawPitchRoll       = ez.sensor.read_yaw_pitch_roll();
-	Qtn                = ez.sensor.read_attitude_quaternion();
-	AngularRate        = ez.sensor.read_angular_rate_measurements();
+def get_num_satellites():
+	return vnSensor.read_gps_solution_lla().num_sats;
 
-	print(getmembers(ez.sensor.read_ins_solution_ecef()));
-	#print(ez.sensor.read_angular_rate_measurements());
-	#Con (22)
-	#print(cd.has_any_position)
-	#print("Any pos [deg,deg,m]: ",cd.any_position)
-	#print(cd.has_yaw_pitch_roll)
-	#print("YPR (Euler angles): ",cd.yaw_pitch_roll)
-	#print(cd.has_any_attitude)
-	#print("Any attitude (?):",cd.any_attitude)
-	#print(cd.has_any_velocity)
-	print("YPR (Euler angles): ", YawPitchRoll);
-	print("Attitude Qtn (?): ", Qtn);
-	print("Angular Rate  (rad/s): ", AngularRate);
-	print("Delta vel (m/s): ", deltaThetaVelocity.delta_velocity);
-	print("Imu Acc (m/s²): ", imuMeasures.accel);
-	print("Imu Gyro (?): ", imuMeasures.gyro);
-	print("Imu Mag (?): ", imuMeasures.mag);
-	print("Imu Pressure (?): ", imuMeasures.pressure);
-	print("Imu Temp (?): ", imuMeasures.temp);
-	#Con (29) incluye todo en (22)
-	#print(cd.has_any_acceleration)
-	#print("Any Acc (m/s²): ",cd.any_acceleration)
-	#print(cd.has_acceleration)
-	#print("Acc (m/s²): ", imuMeasures.accel)
-	#print(cd.has_any_angular_rate)
-	#print("Any ang rate (rad/s): ",cd.any_angular_rate)
-	#print(cd.has_angular_rate)
-	#print("Ang rate (rad/s): ",cd.angular_rate)
-	
-	
-	
+def get_gps_coords():
+	lla    = vnSensor.read_gps_solution_lla();
 	'''
-	#Todo esto es lo que regresa false con la config actual (22) o (29)
-	print("2",cd.has_quaternion)
-	print("3",cd.has_direction_cosine_matrix)
-	print("4",cd.has_any_magnetic)	
-	print("7",cd.has_acceleration_linear_body)
-	print("8",cd.has_acceleration_uncompensated)
-	print("9",cd.has_acceleration_linear_ned)
-	print("10",cd.has_acceleration_linear_ecef)
-	print("11",cd.has_acceleration_ned)
-	print("12",cd.has_acceleration_ecef)	
-	print("15",cd.has_angular_rate_uncompensated)
-	print("16",cd.has_any_pressure)	
-	print("18",cd.has_position_gps_lla)	
-	print(cd.has_temperature)
-	print(cd.has_velocity_gps_ned)
-	print(cd.has_delta_theta)
-	print(cd.has_delta_velocity)
+	'gps_fix', 'lla', 'ned_acc', 'ned_vel', 'num_sats', 'speed_acc', 'this', 'time', 'time_acc', 'week'
 	'''
-	
-	print("")
-	
-	'''
-	print("Yaw , Pitch , Roll" , vnSensor.read_yaw_pitch_roll())
-	print("Attitude Quaternion " , vnSensor.read_attitude_quaternion())
-	
-	#print("Quat, Magn Acc, Angular " ,vnSensor.read_quaternion_magnetic_acceleration_and_angular_rates())
-	print("Magn Measurements ", vnSensor.read_magnetic_measurements())
-	print("Acc Measurements  ", vnSensor.read_acceleration_measurements())
-	print("Ang rates Measurements ", vnSensor.read_angular_rate_measurements())
-	#print("Mag acc and Ang rates", vnSensor.read_magnetic_acceleration_and_angular_rates())
-	#print("Mag and Grav Ref Vec" , vnSensor.read_magnetic_and_gravity_reference_vectors())
+	return {
+		'latitude': lla.lla.x,
+		'longitud': lla.lla.y
+	};
 
-	print("")
-	print("Info: " , compData.yaw_pitch_roll)
-	print("")
-	'''
-	
-	#vnSensor.disconnect()
-	ez.disconnect()
-	
-############################################################################
-def open_communication():
-	if(var.imuPort == ''):
-		print('Error : GetSerialPort must be called before this function')
-	else:
-		vnSensor.connect(var.imuPort, 115200)
+def get_yaw_orientation():
+	return vnSensor.read_yaw_pitch_roll().x;
 
+def get_gps_acceleration_velocity():
+	lla = vnSensor.read_gps_solution_lla();
 
+	return {
+		'acceleration': lla.ned_acc,
+		'velocity': lla.ned_vel
+	}
 
-def close_communication():
-		vnSensor.disconnect()
-'''
-		
+def get_angular_rates():
+	return vnSensor.read_angular_rate_measurements();
 
 def get_acceleration():
-	if(vnSensor.is_connected()):
-		return vnSensor.read_acceleration_measurements()
-	else:
-		print("VnSensor Is not connected")
+	return vnSensor.read_acceleration_measurements();
 
-def get_angular():
-	if(vnSensor.is_connected()):
-		return vnSensor.read_angular_rate_measurements()
-	else:
-		print("VnSensor Is not connected")
-	
-'''
+def get_delta_theta():
+	return vnSensor.read_delta_theta_and_delta_velocity().delta_theta;
+
+def get_delta_velocity():
+	return vnSensor.read_delta_theta_and_delta_velocity().delta_velocity;
+
+
+def get_degrees_to_north_orientation():
+	degree = (get_yaw_orientation()%360) - NORTH_YAW;
+
+	if (degree > 180):
+		degree = degree - 360;
+
+	return degree;
--- vantec.py
+++ vantec.py
@@ -9,7 +9,15 @@ import lib.lidar as lidar
 utility.get_serial_ports()
 #motors.open_communication()
 #motors.test_motors('b', 128 , 128)
-imu.test_function()
 #motors.open_communication()
 #motors.move_thrusters(var.rightThruster, 1400)
 #motors.move_servo(var.leftServo, 120)
+imu.init()
+#print(imu.get_gps_coords());
+#print(imu.get_yaw_orientation());
+#print(imu.get_gps_acceleration_velocity());
+#print(imu.get_angular_rates());
+#print(imu.get_acceleration());
+#print(imu.get_delta_theta());
+#print(imu.get_delta_velocity());
+print(imu.get_degrees_to_north_orientation());
