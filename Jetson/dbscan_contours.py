48895ab5e47e681e5343bf7b6878a2756d6783b9
GabrielGlzSa <gabriel_glzsahagun@hotmail.com>
2017-06-13 02:31:17 -0500

    DBSCAN now returns a list of buoys with distance and degrees.

--- Jetson/dbscan_contours.py
+++ Jetson/dbscan_contours.py
@@ -5,10 +5,19 @@ import math
 import time
 tecla=-1
 
+minRadius=30
 buoy_diameter=20.32 #cm
-def DBSCAN(array, epsylon, minPts):
+def DBSCAN(array, epsylon, minPts,blu=False):
 	start_time = time.time()
-	contours,_=cv2.findContours(array,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	todos_contornos,_=cv2.findContours(array,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+	contours=[]
+	for contorno in todos_contornos:
+		area=cv2.contourArea(contorno)
+		if area>minPts:
+			contours.append(contorno)
+
+	
 	obstacles=np.zeros(array.shape,dtype=np.uint8)
 	print 'Contornos en la imagen: ',len(contours)
 	if len(contours)<1:
@@ -55,7 +64,7 @@ def DBSCAN(array, epsylon, minPts):
 	
 			distance_matrix[A][A]=999
 
-		print(distance_matrix)
+		#print(distance_matrix)
 
 		links=np.argwhere(distance_matrix<epsylon)
 	
@@ -112,8 +121,15 @@ def DBSCAN(array, epsylon, minPts):
 				(x,y),radius = cv2.minEnclosingCircle(enclose)
 				center = (int(x),int(y))
 				radius = int(radius)
+				if radius>40:
+					continue
+				if blu:
+					
+					print center,radius
+					if y<240 and radius>20:
+						continue
 				cv2.circle(obstacles,center,radius,255,2)
-		
+				
 
 	elapsed_time = time.time() - start_time
 	print 'Elapsed Time ',elapsed_time
@@ -139,11 +155,12 @@ hsblue=np.array([ 107.19552311,  184.64772921,  102.44292567])
 hsblue=np.array([ 109.66161975,  218.94410753,  164.98564576])
 
 
-path='/home/ubuntu/Roboboat/fotos/'
-files=os.listdir(path)
+#path='/home/ubuntu/RoboBoat_2017_Main/Jetson/fotos/'
+#files=os.listdir(path)
 
-for image_file in files:
-	image=cv2.imread(path+image_file)
+
+def get_obstacles(image):
+	#image=cv2.imread(path+image_file)
 		
 	
 	azul=cv2.inRange(image,bl, bu)
@@ -162,48 +179,108 @@ for image_file in files:
 	blue=green.copy()
 	red=blue.copy()
 
+	'''
+	#Circle detection
+	minVal=0
+	maxVal=47
+	gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	canny=cv2.Canny(gray,minVal,maxVal,True)
+	contours,_=cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	contour_list=[]
+	for contour in contours:
+		area=cv2.contourArea(contour)
+		#print(area)
+		if (area>50):
+			approx=cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True)
+			#print len(approx)
+			if len(approx)>14:
+				contour_list.append(contour)
+				#np.save('circle',contour)
+
+	value=np.load('circle.npy')
+	image3=np.full(image.shape,255,dtype=np.uint8)
+	circulos=[]
+	for contorno in contour_list:
+		parecido=cv2.matchShapes(value,contorno,1,0.0)
+		#print(parecido)
+		if parecido<0.1:
+			cv2.drawContours(image3,contorno,-1,(255,0,0),2)
+			circulos.append(contorno)
+
+	dentroCircle=np.zeros(image.shape,dtype=np.uint8)
+	bindex=np.nonzero(azul)
+	if(len(bindex[0])>1):
+		tamano=len(circulos)
+		blue_obstacle=np.argwhere(azul==255)
+		if tamano==1:
+			for punto in blue_obstacle:
+				#print(circulos[0][0][0])
+				#print punto
+				dist=cv2.pointPolygonTest(circulos[0],(punto[1],punto[0]),True)
+				if dist>=0:
+					cv2.drawContours(dentroCircle,circulos,-1,(255,255,255),2)
+		elif tamano>1:
+			for circulo in circulos: 
+				for punto in blue_obstacle:
+					dist=cv2.pointPolygonTest(circulo,punto,True)
+					if dist>=0:
+						cv2.drawContours(dentroCircle,contorno,-1,(0,0,0),2)
+	cont=np.zeros(image.shape,dtype=np.uint8)
+	cont=cv2.bitwise_or(cont,dentroCircle)
+	cv2.imshow('Boyas',cont)
+	'''
+
+	#Here ends circle detection and starts color detection
+
+	obstacles=np.zeros((h,w),dtype=np.uint8)
 	yindex=np.nonzero(amarillo)
 	if (len(yindex[0])>1):
 		yellow=DBSCAN(amarillo,epsy,size)
-	
+		obstacles=np.bitwise_or(obstacles,yellow)
+
 	gindex=np.nonzero(verde)
 	if (len(gindex[0])>1):
 		green=DBSCAN(verde,epsy,size)
+		obstacles=np.bitwise_or(obstacles,green)
 
-	bindex=np.nonzero(azul)
-	if (len(bindex[0]>1)):
-		ok=DBSCAN(azul,epsy,size)
-	
-	
 	rindex=np.nonzero(rojo)
 	if (len(rindex[0])>1):
 		red=DBSCAN(rojo,epsy,size)
+		#obstacles=np.bitwise_or(obstacles,red)
+
+	image3=np.zeros(image.shape,dtype=np.uint8)
+	dentroCircle=np.zeros(image.shape,dtype=np.uint8)
+	bindex=np.nonzero(azul)
+	if (len(bindex[0]>1)):
+		blue=DBSCAN(azul,epsy,size,True)
+		#obstacles=np.bitwise_or(obstacles,blue)
+		
 	
-	obstacles=np.bitwise_or(yellow,green)
-	obstacles=np.bitwise_or(obstacles,blue)
-	obstacles=np.bitwise_or(obstacles,red)
+	
+	
+	
 	cv2.imshow('obstacles',obstacles)
 	
 	contours,_=cv2.findContours(obstacles,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	print len(contours)
+	#print len(contours)
+
+	found_obstacles[]
 	for contorno in contours:
+		M1 = cv2.moments(contorno)
+		if (M1['m00']==0):
+			M1['m00']=1
+		cx1 = int(M1['m10']/M1['m00'])
+		cy1 = int(M1['m01']/M1['m00'])
 		rect=cv2.boundingRect(contorno)
-		print rect
 		x=int(rect[0])
 		y=int(rect[1])
 		dx=int(rect[2])
 		dy=int(rect[3])
 		copia=image.copy()
 		cv2.rectangle(image,(x,y),(x+dx,y+dy),(0,0,255),2,8)
-		M1 = cv2.moments(contorno)
-		if (M1['m00']==0):
-			M1['m00']=1
-		cx1 = int(M1['m10']/M1['m00'])
-		cy1 = int(M1['m01']/M1['m00'])
-		print h,w		
+	
+		#print h,w		
 		dpp=float(69)/h
-		print('Degres Per Pixel: ',dpp)
-		print('Centroid: ',cx1,cy1)	
 		
 		if cx1>w/2:
 			pixels=cx1-w/2
@@ -211,20 +288,19 @@ for image_file in files:
 			pixels=-(w/2-cx1)
 
 		degrees=dpp*pixels
-		print("Degrees: ",degrees)
+		
 
 		distance=109 	#Distance to buoy
 		pixel_width=dx	#Pixel
 		W=20.32 		#Real Width
 		F=697.34		#Focal length
 		distance=(W*F)/pixel_width
+		'''
+		print('Centroid: ',cx1,cy1)	
+		print("Degrees: ",degrees)
 		print ('Distance to buoy (cm): ',distance)
-		
+		'''
 
-	cv2.line(image,(w/2,0),(w/2,h-1),(0,0,255),4,8)
-
-	cv2.imshow("image",image)
-	tecla=cv2.waitKey(0)
+		found_obstacles.append([distance,degrees])
 	
-	if tecla==1048689:
-		break
+	return found_obstacles	
