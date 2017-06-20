ef680b4398f2f4f8f9504f9abe3454e51734dfff
aguilerap.jc <marcogil93@gmail.com>
2017-06-20 13:21:24 -0500

    taranis test

--- ArduinoFiles/testing/TaranisTesting/TaranisTesting.ino
+++ ArduinoFiles/testing/TaranisTesting/TaranisTesting.ino
@@ -1,7 +1,7 @@
 //Receiver arduino pins
 const int PIN_X8R_4 = 6;
 const int PIN_X8R_2 = 3;
-const int PIN_X8R_5 = 9;
+const int PIN_X8R_5 = 5;
 
 float channel4;
 float channel2;
@@ -10,23 +10,23 @@ float channel5;
 void setup(){
   pinMode(PIN_X8R_4,INPUT);
   pinMode(PIN_X8R_2,INPUT);
-  pinMode(PIN_X8R_5,INPUT);
-
-  delay(1000);
+  pinMode(PIN_X8R_5,INPUT);  
   Serial.begin(115200);
 }
 
 void loop(){
+  channel4 = pulseIn(PIN_X8R_4, HIGH);
+  channel2 = pulseIn(PIN_X8R_2, HIGH);
+  channel5 = pulseIn(PIN_X8R_5, HIGH);
   //channel4 = map(pulseIn(PIN_X8R_4, HIGH), 1150, 2650, 900,2025);
   Serial.print("Palanca Izquierda ");
-  Serial.println(pulseIn(PIN_X8R_4, HIGH));
+  Serial.println(channel4);
   //channel2 = map(pulseIn(PIN_X8R_2, HIGH), 1150, 2650, 900,2025);
   Serial.print("Palanca Derecha ");
-  Serial.println(pulseIn(PIN_X8R_2, HIGH));
+  Serial.println(channel2);
   //channel5 = map(pulseIn(PIN_X8R_5, HIGH), 1150, 2650, 900,2025);
   Serial.print("Palanca Arriba ");
-  Serial.println(pulseIn(PIN_X8R_5, HIGH));
-  delay(2000);
-  }
+  Serial.println(channel5);
+}
 
 
